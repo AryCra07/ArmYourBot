bb4250cda98d7b7a3053cd443e43e694ea47b9d6
AryCra07 <703810491@qq.com>
2022-08-08 21:33:44 +0800

    完善后端

--- backend/src/main/java/com/ayb/backend/BackendApplication.java
+++ backend/src/main/java/com/ayb/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.ayb.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = "com.ayb.backend.controller")
 public class BackendApplication {
 
     public static void main(String[] args) {
