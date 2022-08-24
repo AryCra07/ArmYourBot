b80b1c2abafd919c8c762026edbfa0851a3d12d2
AryCra07 <703810491@qq.com>
2022-08-24 16:44:06 +0800

    update backend

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,9 +1,9 @@
-package com.ayb.backend;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(scanBasePackages = "com.ayb.backend.controller")
+@SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
