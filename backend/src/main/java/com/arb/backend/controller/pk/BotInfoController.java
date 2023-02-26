8aaafce19259cb36c2acaeb00372fb847e43499a
AryCra07 <703810491@qq.com>
2023-02-27 00:04:42 +0800

    create backend and readme

---
+++ backend/src/main/java/com/arb/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,21 @@
+package com.arb.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+    @RequestMapping("getbotinfo/")
+
+    public List<String> getBotInfo() {
+        List<String> list = new LinkedList<>();
+        list.add("sword");
+        list.add("apple");
+        list.add("lion");
+        return list;
+    }
+}
