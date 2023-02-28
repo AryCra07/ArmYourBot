2ae18142035740f95450465c4f2a9370803eb693
AryCra07 <703810491@qq.com>
2023-02-28 10:06:49 +0800

    update backend

--- backend/src/main/java/com/arb/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/arb/backend/controller/pk/BotInfoController.java
@@ -3,19 +3,26 @@ package com.arb.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
 
-    public List<String> getBotInfo() {
-        List<String> list = new LinkedList<>();
-        list.add("sword");
-        list.add("apple");
-        list.add("lion");
+    public List<Map<String, String>> getBotInfo() {
+        List<Map<String, String>> list = new LinkedList<>();
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+        Map<String, String> bot2 = new HashMap<>();
+        bot2.put("name", "leo");
+        bot2.put("rating", "1800");
+        list.add(bot1);
+        list.add(bot2);
         return list;
     }
 }
