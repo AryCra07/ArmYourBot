88b490e19e2aecdc90e116f676bcf0ce1aec956c
AryCra07 <703810491@qq.com>
2023-03-06 09:24:03 +0800

    update backend: add security mode

---
+++ backend/src/main/java/com/ayb/backend/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.ayb.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- backend/src/main/java/com/ayb/backend/controller/user/UserController.java
+++ backend/src/main/java/com/ayb/backend/controller/user/UserController.java
@@ -5,6 +5,8 @@ import com.ayb.backend.pojo.User;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +37,9 @@ public class UserController {
             @PathVariable String username,
             @PathVariable String password
     ) {
-        User user = new User(userId, username, password);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User(userId, username, encodedPassword);
         userMapper.insert(user);
         return "Add user successfully!";
     }
